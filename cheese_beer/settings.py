67197984a70bfa1d83127f96318d7dd71d1223fd
rowlandcoping <rowlandcoping@gmail.com>
2024-03-28 02:26:47 +0000

    FIX: adjusted settings.py due to incorrect static file settings

--- cheese_beer/settings.py
+++ cheese_beer/settings.py
@@ -71,6 +71,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -168,7 +169,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
@@ -179,7 +180,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SITE_ID=1
 
 #email
+
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+
+# whitenoise
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 
