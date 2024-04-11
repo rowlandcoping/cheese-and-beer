887acaaadc9f7bb487170bdd0ee2a08f1b4596a1
rowlandcoping <rowlandcoping@gmail.com>
2024-04-11 01:24:26 +0100

    ADD: set up basic webhook infrastructure and stripe CLI for testing

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .webhooks import webhook
 from . import views
 
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('confirmation/<order_number>', views.confirmation, name='confirmation'),
+    path('wh/', webhook, name='webhook'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
